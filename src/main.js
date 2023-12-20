9846aabd1ea2069399872cbd733cee6a7b1dc5e8
doxxen <zamoranoezequiel3@gmail.com>
2023-12-19 21:28:10 -0300

    listo

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ import store from './store'
 import { BootstrapVue } from 'bootstrap-vue'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
-import { SpeedInsights } from "@vercel/speed-insights/vue"
 
-const socketConnection = SocketIO('http://localhost:4201');
+const socketConnection = SocketIO('tcp://0.tcp.sa.ngrok.io:15480');
 
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -17,7 +16,7 @@ Vue.config.productionTip = false
 Vue.prototype.$paises = ['Argentina','Chile','Peru','Bolivia','Colombia'],
 Vue.config.productionTip = false
 Vue.prototype.$envio = 20;
-Vue.prototype.$url = 'http://localhost:4201/api';
+Vue.prototype.$url = 'tcp://0.tcp.sa.ngrok.io:15480';
 Vue.prototype.$token = localStorage.getItem('token');
 Vue.use (BootstrapVue);
 Vue.use(new VueSocketIO({
